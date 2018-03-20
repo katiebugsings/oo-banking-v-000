f05e477d33805f36a185cb4b0fcb7a8891cbc34d
Katie Cassell <kcassell944@gmail.com>
2018-03-20 18:29:53 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,14 @@
 class BankAccount
+attr_accessor :balance, :status, :name, :deposit
+
+def initialize(name, deposit)
+  @name = name
+  @deposit = deposit
+  @balance = 1000
+  @status = "open"
+end
+
+def deposit
+  end
 
 end
