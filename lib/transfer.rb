36eef8ec03279e6778e5075b7cb57f1fefaf16da
Katie Cassell <kcassell944@gmail.com>
2018-03-20 19:06:06 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,10 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :status, :amount
+
+  def initialize(sender)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
 end
